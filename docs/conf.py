48a5fd0af30b03b6e06ef88d08b90c6e7ff71b3e
TimHenry1995 <52284098+TimHenry1995@users.noreply.github.com>
2023-06-07 12:38:16 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,8 @@ autodoc_default_options = {
 import os, sys
 for x in os.walk('.'):
   sys.path.insert(0, x[0])
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath(os.path.join("..","..")))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information

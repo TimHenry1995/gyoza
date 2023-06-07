589dcade7f91df31bc5d479caaa0696c001f29bb
TimHenry1995 <52284098+TimHenry1995@users.noreply.github.com>
2023-06-07 15:29:03 +0200

    Added and tested expand_axes

--- src/gyoza/utilities/tensors.py
+++ src/gyoza/utilities/tensors.py
@@ -20,3 +20,40 @@ def move_axis(x: tf.Tensor, from_index: int, to_index: int) -> tf.Tensor:
 
     # Outputs
     return x_new
+
+def expand_axes(x: tf.Tensor, axes) -> tf.Tensor:
+    """Expands x with singleton axes.
+    
+    :param x: The tensor to be expanded.
+    :type x: :class:`tensorflow.Tensor`
+    :param axes: The axes along which to expand. Their indices are assumed to be valid in the output shape. They must not 
+        introduce gaps in the output shape. This means if, e.g. x has two axes then ``axes`` may be, e.g. [0,1,3,5,6,7] where
+        axes 2 and 4 are filled in orde by x but ``axes`` must not be, e.g. [0,1,3,5,6,10] because of the gap between 6 and 10 
+        that would be introduced in the output shape.
+    :type axes: :class:`List[tensorflow.Tensor]`
+    
+    :return: x_new (:class:`tensorflow.Tensor`) - The reshaped version of x with singletons along ''axes''."""
+
+    # Initialize
+    new_axis_count = len(x.shape) + len(axes)
+    
+    # Compatibility of new and old axes
+    old_axes = list(range(new_axis_count))
+    for axis in axes:
+        # Input validity
+        assert axis < new_axis_count, f"""The axis {axis} must be in the interval [0,{new_axis_count})."""
+    
+        # Exclude new axis from old axes
+        old_axes.remove(axis)
+
+    # Set new shape
+    o = 0 # Iterates old axes
+    new_shape = [1] * new_axis_count
+    for axis in old_axes:
+        new_shape[axis] = x.shape[o]
+        o += 1
+
+    x_new = tf.reshape(x, new_shape)
+
+    # Outputs
+    return x_new
--- unit_tests/utilities/tensors_test.py
+++ unit_tests/utilities/tensors_test.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 class Test_Move_Axis(unittest.TestCase):
 
     def test_same_axis(self):
-        """Test whether the move_axis method manages to move the axis to its current position.
+        """Test whether the move_axis function manages to move the axis to its current position.
         This is an identity operation."""
 
         # Initialize
@@ -21,7 +21,7 @@ class Test_Move_Axis(unittest.TestCase):
         self.assertTupleEqual(tuple1=tuple(x_target.shape), tuple2=tuple(x_observed.shape))
 
     def test_axes_1_3(self):
-        """Test whether the move_axis method manages to move the axis from index 1 to 3."""
+        """Test whether the move_axis function manages to move the axis from index 1 to 3."""
 
         # Initialize
         x = tf.reshape(tf.range(0,24), [1,2,3,4])
@@ -36,7 +36,7 @@ class Test_Move_Axis(unittest.TestCase):
         self.assertTupleEqual(tuple1=tuple(x_target.shape), tuple2=tuple(x_observed.shape))
 
     def test_axes_2_0(self):
-        """Test whether the move_axis method manages to move the axis from index 2 to 0."""
+        """Test whether the move_axis function manages to move the axis from index 2 to 0."""
 
         # Initialize
         x = tf.reshape(tf.range(0,24), [1,2,3,4])
@@ -46,6 +46,83 @@ class Test_Move_Axis(unittest.TestCase):
 
         # Observe
         x_observed = utt.move_axis(x=x, from_index=2, to_index=0)
+
+        # Evaluate
+        self.assertTupleEqual(tuple1=tuple(x_target.shape), tuple2=tuple(x_observed.shape))
+
+class Test_Expand_Axes(unittest.TestCase):
+
+    def test_axes_0(self):
+        """Test whether the expand_axes function manages to expand along one axis"""
+
+        # Initialize
+        x = tf.reshape(tf.range(0,24), [2,3,4])
+
+        # Target
+        x_target = tf.reshape(x, [1,2,3,4])
+
+        # Observe
+        x_observed = utt.expand_axes(x=x, axes=[0])
+
+        # Evaluate
+        self.assertTupleEqual(tuple1=tuple(x_target.shape), tuple2=tuple(x_observed.shape))
+
+    def test_axes_0_4(self):
+        """Test whether the expand_axes function manages to expand along axes 0 and 4"""
+
+        # Initialize
+        x = tf.reshape(tf.range(0,24), [2,3,4])
+
+        # Target
+        x_target = tf.reshape(x, [1,2,3,4,1])
+
+        # Observe
+        x_observed = utt.expand_axes(x=x, axes=[0,4])
+
+        # Evaluate
+        self.assertTupleEqual(tuple1=tuple(x_target.shape), tuple2=tuple(x_observed.shape))
+
+    def test_axes_4_0(self):
+        """Test whether the expand_axes function manages to expand along axes 4 and 0"""
+
+        # Initialize
+        x = tf.reshape(tf.range(0,24), [2,3,4])
+
+        # Target
+        x_target = tf.reshape(x, [1,2,3,4,1])
+
+        # Observe
+        x_observed = utt.expand_axes(x=x, axes=[4,0])
+
+        # Evaluate
+        self.assertTupleEqual(tuple1=tuple(x_target.shape), tuple2=tuple(x_observed.shape))
+
+    def test_axes_3_0(self):
+        """Test whether the expand_axes function manages to expand along axes 3 and 0"""
+
+        # Initialize
+        x = tf.reshape(tf.range(0,24), [2,3,4])
+
+        # Target
+        x_target = tf.reshape(x, [1,2,3,1,4])
+
+        # Observe
+        x_observed = utt.expand_axes(x=x, axes=[3,0])
+
+        # Evaluate
+        self.assertTupleEqual(tuple1=tuple(x_target.shape), tuple2=tuple(x_observed.shape))
+
+    def test_axes_3_2_0(self):
+        """Test whether the expand_axes function manages to expand along axes 3, 2 and 0"""
+
+        # Initialize
+        x = tf.reshape(tf.range(0,24), [2,3,4])
+
+        # Target
+        x_target = tf.reshape(x, [1,2,1,1,3,4])
+
+        # Observe
+        x_observed = utt.expand_axes(x=x, axes=[3,2,0])
 
         # Evaluate
         self.assertTupleEqual(tuple1=tuple(x_target.shape), tuple2=tuple(x_observed.shape))

fb5b06737f5599b0ba77d0e8028c47f6634ea27c
TimHenry1995 <52284098+TimHenry1995@users.noreply.github.com>
2023-06-09 12:34:42 +0200

    squarewave2D mask now tested

--- unit_tests/modelling/semi_selectors_test.py
+++ unit_tests/modelling/semi_selectors_test.py
@@ -206,7 +206,67 @@ class TestSquareWave1D(unittest.TestCase):
 class TestSquareWave2D(unittest.TestCase):
 
     def test_mask_two_dimensional_axes_0_1(self):
-        
+        """Tests whether the mask method of SquareWave2D works on a two dimensional input along axes 0 and 1."""
+
+        # Initialize
+        x = tf.reshape(tf.range(15, dtype=tf.float32), shape=[3,5])
+
+        # Target
+        x_target = x.numpy()
+        x_target[0,::2] = 0
+        x_target[1,1::2] = 0
+        x_target[2,::2] = 0
+        x_target = tf.constant(x_target)
+
+        # Observe
+        instance = mss.SquareWave2D(axes=[0,1], shape=[3,5])
+        x_observed = instance.mask(x=x)
+
+        # Evaluate
+        self.assertTupleEqual(tuple1=tuple(x_target.shape), tuple2=tuple(x_observed.shape))
+        self.assertEqual(first=tf.reduce_sum((x_target-x_observed)**2).numpy(), second=0)
+
+    def test_mask_three_dimensional_axes_1_2(self):
+        """Tests whether the mask method of SquareWave2D works on a three dimensional input along axes 1 and 2."""
+
+        # Initialize
+        x = tf.reshape(tf.range(30, dtype=tf.float32), shape=[2,3,5])
+
+        # Target
+        x_target = x.numpy()
+        x_target[:,0,::2] = 0
+        x_target[:,1,1::2] = 0
+        x_target[:,2,::2] = 0
+        x_target = tf.constant(x_target)
+
+        # Observe
+        instance = mss.SquareWave2D(axes=[1,2], shape=[3,5])
+        x_observed = instance.mask(x=x)
+
+        # Evaluate
+        self.assertTupleEqual(tuple1=tuple(x_target.shape), tuple2=tuple(x_observed.shape))
+        self.assertEqual(first=tf.reduce_sum((x_target-x_observed)**2).numpy(), second=0)
+
+    def test_mask_four_dimensional_axes_1_2(self):
+        """Tests whether the mask method of SquareWave2D works on a three dimensional input along axes 1 and 2."""
+
+        # Initialize
+        x = tf.reshape(tf.range(60, dtype=tf.float32), shape=[2,3,5,2])
+
+        # Target
+        x_target = x.numpy()
+        x_target[:,0,::2,:] = 0
+        x_target[:,1,1::2,:] = 0
+        x_target[:,2,::2,:] = 0
+        x_target = tf.constant(x_target)
+
+        # Observe
+        instance = mss.SquareWave2D(axes=[1,2], shape=[3,5])
+        x_observed = instance.mask(x=x)
+
+        # Evaluate
+        self.assertTupleEqual(tuple1=tuple(x_target.shape), tuple2=tuple(x_observed.shape))
+        self.assertEqual(first=tf.reduce_sum((x_target-x_observed)**2).numpy(), second=0)
 
 if __name__ == "__main__":
     unittest.main()

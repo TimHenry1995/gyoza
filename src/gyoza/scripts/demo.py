24ca217e8608706e08ff9c7ebfa1d45b5a6bc892
TimHenry1995 <52284098+TimHenry1995@users.noreply.github.com>
2023-06-17 14:43:32 +0200

    saved example model

--- src/gyoza/scripts/demo.py
+++ src/gyoza/scripts/demo.py
@@ -5,24 +5,36 @@ from gyoza.modelling import standard_layers as msl
 import os
 
 if __name__ == "__main__":
+    # TODO Swirl example with two spatial axes and 1 channel axis
+    def create_model(channel_count: int = 5) -> msl.FlowLayer:
+
+        compute_coupling_parameters = tf.keras.layers.Dense(units=channel_count)
+        mask = gmm.HeaviSide(axes=[1], shape=[channel_count])
+        
+        network = gmf.SequentialFlowNetwork(sequence=[
+            gmf.AdditiveCoupling(axes=[1], shape=[channel_count], compute_coupling_parameters=compute_coupling_parameters, mask=mask), 
+            gmf.Shuffle(axes=[1], shape=[channel_count])
+            ])
+
+        return network
+
     channel_count = 5
     batch_size = 4
-    compute_coupling_parameters = tf.keras.layers.Dense(units=channel_count)
-    mask = gmm.HeaviSide(axes=[1], shape=[channel_count])
-    mask.build(input_shape=[batch_size,channel_count])
-    #coupling_layer =  msl.BasicFullyConnectedNet(latent_channel_count=channel_count, output_channel_count=channel_count, depth=3)
-    coupling_layer = gmf.AdditiveCouplingLayer(compute_coupling_parameters=compute_coupling_parameters, mask=mask)
+
+    network = create_model(channel_count=channel_count)
 
     x = tf.reshape(tf.range(batch_size*channel_count, dtype=tf.float32), [batch_size, channel_count])
-    y = coupling_layer(x)
+    y = network(x)
     print(x)
     print(y)
 
     # Saving and Loading
-    path = os.path.join(os.getcwd(), "example_model")
-    coupling_layer.save(path)
-    del coupling_layer
-    loaded_coupling_layer = tf.keras.models.load_model(path)
-    y_prime = loaded_coupling_layer(x)
+    path = os.path.join(os.getcwd(), "example_model.h5")
+    network.save_weights(path)
+    del network
+    loaded_network = create_model()
+    loaded_network.build(input_shape=x.shape)
+    loaded_network.load_weights(path)
+    y_prime = loaded_network(x)
     print(y_prime)
     k=3
